4c5e8fa086474b40ce6d5eee16b7578f6decc323
mschubert <mschu.dev@gmail.com>
2014-09-04 18:51:41 +0100

    fix #8, comment duplicate definition of %or%

--- base/operators.r
+++ base/operators.r
@@ -1,20 +1,31 @@
 # some operators for R
 
 `%or%` = function(a, b) {
-    if (identical(a, FALSE) || is.null(a) || is.na(a) || is.nan(a) || length(a) == 0) b else a
+    cmp = function(a,b) if (identical(a, FALSE) || 
+                            is.null(a) || 
+                            is.na(a) || 
+                            is.nan(a) || 
+                            length(a) == 0) b else a
+
+    if (is.list(a))
+        mapply(cmp, a, b, SIMPLIFY=F)
+    else if (is.vector(a))
+        mapply(cmp, a, b)
+    else
+        cmp(a, b)
 }
 
 `%OR%` = function(a, b) {
     tryCatch(
         a %or% b,
-        error = function(cond) b
+        error = function(e) b
     )
 }
 
 `%catch%` = function(a, b) {
     tryCatch(
         a,
-        error = function(cond) b
+        error = function(e) b
     )
 }
 
