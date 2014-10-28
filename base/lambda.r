30a3fc208919d5f83e4eee0be09a71c8d92ce86d
Konrad Rudolph <konrad.rudolph@gmail.com>
2014-10-28 17:02:28 +0000

    Improve and correct documentation

--- base/lambda.r
+++ base/lambda.r
@@ -2,12 +2,16 @@ fun = import('./functional')
 
 #' Define a single-argument function
 #'
-#' @note \textbf{Warning} This breaks \code{<-} assignment because it overloads
-#' the operator. \code{=} assignment \textbf{must} be used instead.
+#' @param params the formal argument names, separated by \code{~}, and
+#' optionally surrounded by \code{(…)}
+#' @param body an expression to be executed, same as a \code{function}’s body
+#'
+#' @note \strong{Warning} This breaks \code{<-} assignment because it overloads
+#' the operator. \code{=} assignment \strong{must} be used instead.
 #' The rationale for introducing this redefinition and breaking existing code is
 #' two-fold:
 #'
-#' \begin{enumerate}
+#' \enumerate{
 #'  \item R is a functional programming language, yet has no concise way of
 #'      defining an anonymous function. This seriously prevents good code. There
 #'      are ways of alleviating this (e.g. by using the \code{.} function) but
@@ -18,7 +22,19 @@ fun = import('./functional')
 #'      single compelling argument for this preference, other than history, and
 #'      the need for concise lambdas clearly overrides a whimsical preference of
 #'      one redundant operator over another.
-#' \end{enumerate}
+#' }
+#'
+#' @usage params -> body
+#' @name lambda
+#' @aliases ->
+#'
+#' @examples
+#' \dontrun{
+#' sapply(1 : 4, x -> 2 * x)
+#' # [1] 2 4 6 8
+#' mapply(x ~ y -> x + y, 1 : 4, 5 : 8)
+#' # [1]  6  8 10 12
+#' }
 `<-` = function (body, params) {
     vars = all.vars(substitute(params))
     formals = as.pairlist(setNames(replicate(length(vars), quote(expr = )),
