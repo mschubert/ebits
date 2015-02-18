6a27f80e40d62ac875aec19ff0c7308d5f0f6690
mschubert <mschu.dev@gmail.com>
2015-02-18 15:45:28 +0000

    add brew templaces

--- plot/color.r
+++ plot/color.r
@@ -23,3 +23,42 @@ p_effect = function(df, pvalue="pvalue", effect="effect", dir=1, thresh=0.05) {
     df[[effect]] = sign(dir) * df[[effect]]
     df
 }
+
+#' Shows available colour palettes, either as plot or return value
+palettes = function() {
+    tryCatch(
+        RColorBrewer::display.brewer.all(),
+        warning = function(x) RColorBrewer::brewer.pal.info
+    )
+}
+
+#' @param df        Main data frame
+#' @param value     Field name of variable to derive colours from
+#' @param color     Field name of variable to save colours to
+#' @param palette   ColorBrewer palette to use
+#' @param na        Colour to use if value is NA
+#' @param quantize  Sequence of breaks - e.g.: `seq(-1,1,by=0.25)` or `FALSE`
+#TODO: alpha?
+brew = function(df, value="value", color="color", palette="RdYlBu", na=NA, quantize=FALSE) {
+    if (identical(quantize, FALSE)) {
+        n_col = 100
+        quantize = do.call(seq.int, c(as.list(range(df[[value]])), list(length.out=n_col)))
+    } else
+        n_col = length(quantize)
+
+    max_cols = RColorBrewer::brewer.pal.info[palette,'maxcolors']
+    if (n_col > max_cols)
+        pal_col = max_cols
+    else
+        pal_col = n_col
+
+    bins = cut(df[[value]], breaks=quantize, include.lowest=TRUE,
+               label=cut(quantize, breaks=quantize)[-1])
+    cols = RColorBrewer::colorRampPalette(brewer.pal(pal_col, palette))(n_col)
+    df[[color]] = cols[as.numeric(bins)]
+    df[[color]][is.na(df[[color]])] = na
+    df
+}
+
+#gradient = function(df, value="value", color="color", low=, mid=, high=, na=NA) {
+#}
