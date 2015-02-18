b7dda8b94d79389da226d4776f0d0b0f1446092b
mschubert <mschu.dev@gmail.com>
2015-02-18 13:23:31 +0000

    refactor for matrix plot

--- plot/matrix.r
+++ plot/matrix.r
@@ -1,24 +1,47 @@
+#TODO:
+#  do not compute cor here, but have column in df
+#  control colour and size, both or separately
+#  either triangular or full
+#  ability to cluster cols/rows
+
 # based on: https://github.com/briatte/ggcorr
-ggcorr = function(data, method = "pairwise", palette = "RdYlGn", name = "rho",
-                  geom = "tile", max_size = 6, label = FALSE, label_alpha = FALSE,
-                  label_color = "black", label_round = 1, ...) {
 
-    M = cor(data[1:ncol(data)], use = method)
+#' @param data         Main data.frame
+#' @param row          Field name for row labels
+#' @param column       Field name for column labels
+#' @param value        Field name for values in data
+#' @param color        Field name for colour
+#' @param name         Title of the legend
+#' @param geom         `circle` or `tile`
+#' @param max_size     maximal size of circles (have this default to tile size, or remove option?)
+#' @param label_color  Color to use for labels (e.g. 'black'), or NULL if no labels
+#' @param label_alpha  Whether to use alpha channel for labels
+matrix = function(data, row="row", col="col", value="value",
+        palette = "RdYlGn", name = "rho", geom = "tile",
+        max_size = 6, label_alpha = FALSE, label_color = "black",
+        quantize = seq(-1, 1, by = .25), ...) {
 
-    # correlation coefficients
-    D = round(M, label_round)
-    D = D * lower.tri(D)
-    D = as.data.frame(D)
-    D = data.frame(row = names(data), D)
-    D = melt(D, id.vars = "row")
-  
-    # correlation quantiles
-    M = M * lower.tri(M)
-    M = as.data.frame(M)
-    M = data.frame(row = names(data), M)
-    M = melt(M, id.vars = "row")
-    M$value[M$value == 0] = NA
-    s = seq(-1, 1, by = .25)
+    M$label = paste(data$indep_var, data$dep_var, sep=":")
+
+    M = data
+    M$value = data[[value]]
+    M$variable = data[[label]]
+    s = quantize
+#    M = cor(data[1:ncol(data)], use = method)
+#
+#    # correlation coefficients TODO: only used to subset labels to triangular
+#    D = round(M, label_round)
+#    D = D * lower.tri(D)
+#    D = as.data.frame(D)
+#    D = data.frame(row = names(data), D)
+#    D = melt(D, id.vars = "row")
+#  
+#    # correlation quantiles
+#    M = M * lower.tri(M)
+#    M = as.data.frame(M)
+#    M = data.frame(row = names(data), M)
+#    M = melt(M, id.vars = "row")
+#    M$value[M$value == 0] = NA
     M$value = cut(M$value, breaks = s, include.lowest = TRUE,
                   label = cut(s, breaks = s)[-1])
     M$row = factor(M$row, levels = unique(as.character(M$variable)))
@@ -48,15 +71,15 @@ ggcorr = function(data, method = "pairwise", palette = "RdYlGn", name = "rho",
             geom_tile(aes(fill = value), colour = "white")
   
     # add coefficient text
-    if (label) {
+    if (!is.null(label_color)) {
         if (label_alpha)
             p = p + 
-                geom_text(data = subset(D, value != 0), 
+                geom_text(data = M, # subset(D, value != 0), 
                     aes(row, variable, label = value, alpha = abs(as.numeric(value))),
                     color = label_color, show_guide = FALSE)
         else
             p = p + 
-                geom_text(data = subset(D, value != 0), 
+                geom_text(data = M, #subset(D, value != 0), 
                     aes(row, variable, label = value),
                     color = label_color)
     }
