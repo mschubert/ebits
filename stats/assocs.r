24ff2c1e50d9b1c1fc791e3f2fdffb51f6e5bbd4
mschubert <mschu.dev@gmail.com>
2015-03-15 15:28:47 +0000

    add return_intercept option

--- stats/assocs.r
+++ stats/assocs.r
@@ -1,25 +1,28 @@
-# linear associations (anova-like)
-
 #' @param formula  A formula that describes the data relationship
 #' @param data     The data to process, or parent.frame() by default
 #' @param min_pts  Minimum number of data points to calculate model from
+#' @param return_intercept  Whether or not to return the intercept
 #' @param ...      Used as variable names and indices along columns;
 #                  this should be needed only if called from df$call
-lm = function(formula, data=parent.frame(), min_pts=3, ...) {
+#' @return         A data.frame with the associations
+lm = function(formula, data=parent.frame(), min_pts=3, return_intercept=FALSE, ...) {
     subs = list(...)
     for (i in seq_along(subs))
         data[[names(subs)[i]]] = data[[names(subs)[i]]][,subs[[i]]]
-    data = as.data.frame(data)
+    data = na.omit(as.data.frame(data))
 
-    if (nrow(data) < min_pts)
-        NULL
-    else
-        cbind(broom::tidy(stats::lm(formula, data=data)), size=nrow(data))
+    if (nrow(data) >= min_pts) {
+        result = cbind(broom::tidy(stats::lm(formula, data=data)), size=nrow(data))
+        if (return_intercept)
+            result
+        else
+            dplyr::filter(result, term != "(Intercept)")
+    }
 }
 
-#.cox = function(formula) {
+#cox = function(formula) {
 #}
 #
 ## this will be somewhat complicated
-#.sem = function(formula) {
+#sem = function(formula) {
 #}
