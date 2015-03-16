6d564154bb1d84a09acc489aa7e1de507efdd531
mschubert <mschu.dev@gmail.com>
2015-03-16 21:51:25 +0000

    split out cleaning func

--- stats/assocs.r
+++ stats/assocs.r
@@ -19,28 +19,34 @@ lm = function(formula, data=parent.frame(), min_pts=3, subsets=NULL, return_inte
     subs = list(...)
     for (i in seq_along(subs))
         data[[names(subs)[i]]] = .b$subset(data[[names(subs)[i]]], index=subs[[i]])
-    data = na.omit(as.data.frame(data))
 
     # define what to do with one subset
     one_lm = function(data) {
-        if (nrow(data) >= min_pts) {
-            result = cbind(broom::tidy(stats::lm(formula, data=data)), size=nrow(data))
-            if (return_intercept)
-                result
-            else
-                dplyr::filter(result, term != "(Intercept)")
-        }
+        pts = nrow(na.omit(do.call(cbind, data))) #FIXME: this will be wrong with matrices
+        if (pts >= min_pts)
+            stats::lm(formula, data=data) %>% .clean_model_result()
     }
 
     # subset as specified in `subsets`
     if (is.null(subsets))
         one_lm(data)
     else
-        lapply(.ar$split(data, along=1, subsets=subsets), one_lm) %>%
+        lapply(data, function(d) .ar$split(d, along=1, subsets=subsets)) %>%
+            .b$list$transpose() %>%
+            lapply(one_lm) %>%
             .df$rbind(add_col="subset")
 }
 
-#TODO: subsets should support df column name
+.clean_model_result = function(model, return_intercept=FALSE, add_size=TRUE) {
+    #TODO: clean multi-response model names
+    model = model %>%
+        broom::tidy() %>%
+        cbind(size = 10) #FIXME: size
+    if (return_intercept)
+        model
+    else
+        dplyr::filter(model, term != "(Intercept)")
+}
 
 #cox = function(formula) {
 #}
