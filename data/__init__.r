ffb927d9f15083c74bd781fecf20fcf16e07a51f
Michael Schubert <mschu.dev@gmail.com>
2015-04-10 10:27:01 +0100

    Delete __init__.r
    otherwise, importing one data module imports all others - we don't want that

--- data/__init__.r
+++
@@ -1,4 +0,0 @@
-# export all submodules using their module names
-.lsdir = list.dirs(module_file(), recursive=FALSE)
-for (dir in .lsdir[file.info(.lsdir)$isdir])
-    assign(basename(dir), import(paste("./", basename(dir), sep="")))
