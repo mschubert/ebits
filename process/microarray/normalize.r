33642294679f2ab576b1c3cffeed51d182d4b975
mschubert <mschu.dev@gmail.com>
2015-05-10 12:06:00 +0100

    support more array types

--- process/microarray/normalize.r
+++ process/microarray/normalize.r
@@ -1,14 +1,22 @@
+.b = import('../../base')
+`%>%` = magrittr::`%>%`
+
 #' Function to normalize expression objects
 #'
 #' @param rawData  A raw data object, or a list thereof
 #' @param method   Method to use for normalization: rma, gcrma, frma
 #' @return         The normalized data
-normalize = function(rawData, method="rma") {
+normalize = function(rawData, ...) {
     UseMethod("normalize")
 }
 
-normalize.list = function(rawData, method="rma") {
-    lapply(rawData, normalize)
+normalize.list = function(rawData, ...) {
+    re = lapply(rawData, function(x) normalize(x, ...) %catch% NA)
+    if (any(is.na(re)))
+        warning("dropping ", names(re)[is.na(re)])
+    if (all(is.na(re)))
+        stop("all normalizations failed")
+    re[!is.na(re)]
 }
 
 normalize.FeatureSet = function(rawData, method="rma") {
@@ -27,4 +35,34 @@ normalize.AffyBatch = function(rawData, method="rma") {
         expr = frma::frma(rawData, target="core")
     else
         stop("invalid method")
+}
+
+normalize.EListRaw = function(rawData, method="quantile") {
+    rawData %>%
+        limma::backgroundCorrect() %>%
+        limma::normalizeBetweenArrays(method=method) %>%
+        limma::avereps(., ID=.$genes$ProbeName)
+}
+
+normalize.RGList = function(rawData, method="loess") {
+    rawData %>%
+        limma::backgroundCorrect() %>%
+        limma::normalizeWithinArrays(method=method) %>%
+        limma::avereps(., ID=.$genes$ProbeName)
+}
+
+normalize.NChannelSet = function(rawData, ...) {
+    ad = as.list(Biobase::assayData(rawData))
+    ad$genes = Biobase::fData(rawData)
+
+    if ("E" %in% names(ad)) # one-color
+        norm = normalize.EListRaw(new("EListRaw", ad), ...)
+    else # two-color
+        norm = normalize.RGList(new("RGList", ad), ...)
+
+    Biobase::fData(rawData) = norm$genes
+    norm$genes = NULL
+    #FIXME: the next line doesn't quite work
+    Biobase::assayData(rawData) = new("list", norm)
+    rawData
 }
