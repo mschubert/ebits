05324e74da4484125db55d5a07ea2b5ac16d8369
mschubert <mschu.dev@gmail.com>
2015-05-12 14:17:49 +0100

    handle subsets in from_formula

--- data_frame/from_formula.r
+++ data_frame/from_formula.r
@@ -14,14 +14,20 @@
 .b = import('base')
 .gfd = import('./get_formula_data')
 
-from_formula = function(formula, data=parent.frame(), group=NULL) {
+#' Gathers all data required for a formula and creates a subsetting index
+#'
+#' @param formula  A standard R formula
+#' @param data     Where to look for the data the `formula` references
+#' @param group    Names of variables that should be column-interated together
+#' @param subsets  How to divide each variable in `formula` along first axis
+from_formula = function(formula, data=parent.frame(), group=NULL, subsets=NULL) {
     pp = .gfd$get_formula_data(formula, data)
     data = lapply(pp$data, as.matrix)
     formula = pp$form
     formula_vars = all.vars(formula)
     matrix_vars = names(data)[sapply(data, ncol) > 1]
 
-    # check groups
+    # allow groups only when they make sense
     if (!is.null(group) && !is.character(group))
         stop("group needs to be NULL or a character vector")
     if (!all(group %in% all.vars(formula)))
@@ -30,19 +36,26 @@ from_formula = function(formula, data=parent.frame(), group=NULL) {
     if (length(diff) > 0)
         stop(paste("Grouped iterations only make sense for matrix vars:", diff))
 
-    # create a data.frame that provides indices for formula data given
-    # it might be easier to construct full df and remove non-groups?
+    # define anchor to iterate only non-grouped variables first
     anchor = group[1]
     grouped = group[2:length(group)]
     ungrouped = setdiff(formula_vars, grouped)
-    index = do.call(.b$expand_grid, sapply(ungrouped, function(x)
+
+    # get all individual indices, then expand.grid around them
+    index_items = sapply(ungrouped, function(x)
         .b$descriptive_index(data[[x]], along=2),
-        USE.NAMES=TRUE, simplify=FALSE)
+        USE.NAMES=TRUE, simplify=FALSE
     )
+    index_items$subsets = unique(subsets)
+    index = do.call(.b$expand_grid, index_items)
+
+    # add grouped items to be the same as the var they are grouped with
     for (var in grouped)
         index[[var]] = index[[anchor]]
 
+    # add data as attribute
     attr(index, "args") = list(data=data, formula=formula)
+    attr(index, "args")$subsets = subsets
     class(index) = append(class(index),"attr_args")
     index
 }
