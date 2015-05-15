3770d33c5a2652b94f113bc9c38b10f3d92f5ea1
mschubert <mschu.dev@gmail.com>
2015-05-15 15:41:00 +0100

    roxygen compliant docs

--- data_frame/rbind.r
+++ data_frame/rbind.r
@@ -2,9 +2,10 @@
 
 #' `rbind` with option to add list names as column
 #'
-#' x_list    A list to bind row-wise
-#' name_col  Name of the column to add list names to
-#' ...       Arguments to be passed to `rbind`
+#' @param x_list    A list to bind row-wise
+#' @param name_col  Name of the column to add list names to
+#' @param ...       Arguments to be passed to `rbind`
+#' @return          A data.frame
 rbind = function(x_list, add_col=NULL, ...) {
     if (!is.null(add_col))
         x_list %<>%
