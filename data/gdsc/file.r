372088cbd9ea0c6ca4eae706dcf409b3927e4792
mschubert <mschu.dev@gmail.com>
2015-05-18 12:25:09 +0100

    add drug filtering

--- data/gdsc/file.r
+++ data/gdsc/file.r
@@ -7,7 +7,8 @@
     DRUG_PROPS = "DRUG_ANALYSIS_SET_20150123.rdata",
     DRUG_IC50 = "dfIC50v17.RData",
     DRUG_AUC = "dfAUCv17.RData",
-    INTOGEN_DRIVERS = "cancer_drivers_5_2_2014.ro"
+    INTOGEN_DRIVERS = "cancer_drivers_5_2_2014.ro",
+    CONC = "screenConcentration.RData"
 )
 
 get = function(id) .p$load('gdsc', .files[[id]])
