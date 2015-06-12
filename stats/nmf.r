e317467163c4f0b303b47e5f595215f54f1d0ed6
mschubert <mschu.dev@gmail.com>
2015-06-12 16:25:24 +0100

    fix: return processed matrices, not initial

--- stats/nmf.r
+++ stats/nmf.r
@@ -4,7 +4,7 @@
 
 #' Wrapper function that takes k, num.clusterings and performs max_iter iterations
 #'
-#' @param A          data matrix
+#' @param A          data matrix [genes x samples]
 #' @param k          the number of clusters (matrix rows)
 #' @param max_iter   number of NMF iterations to perform
 #' @param seed       the random seed
@@ -13,6 +13,8 @@
 #' @return a list of the results matrices W and H, with iterations performed
 .nmf = function(A, k, max_iter, seed=1234, tolerance=1e-4, num_clusterings=10) {
     one_iter = function(seed) {
+        # initialise matrices
+        set.seed(seed)
         m = nrow(A)
         n = ncol(A)
         W = as.double(runif(m*k, 0, 1)) # standard random init in (0,1)
@@ -20,14 +22,15 @@
 
         # calls to add: nmf_als, mu, neals, alspg, pg
         dyn.load("libnmf.so")
-        result = .C("nmf_mu", a=as.double(A), w0=as.double(W), h0=as.double(H),
-                     pm=as.integer(m), pn=as.integer(n), pk=as.integer(k),
-                     maxiter=as.integer(max_iter), pTolX=as.double(tolerance),
-                     pTolFun=as.double(tolerance), DUP=FALSE, PACKAGE='libnmf')
+        re = .C("nmf_mu", a=as.double(A), w0=as.double(W), h0=as.double(H),
+                 pm=as.integer(m), pn=as.integer(n), pk=as.integer(k),
+                 maxiter=as.integer(max_iter), pTolX=as.double(tolerance),
+                 pTolFun=as.double(tolerance), DUP=FALSE, PACKAGE='libnmf')
 
-        list(W = matrix(W, nrow=m, ncol=k, dimnames=list(rownames(A), NULL)),
-             H = matrix(H, nrow=k, ncol=n, dimnames=list(NULL, colnames(A))),
-             iter=result$maxiter)
+        # return decomposed matrices
+        list(W = matrix(re$w0, nrow=m, ncol=k, dimnames=list(rownames(A), NULL)),
+             H = matrix(re$h0, nrow=k, ncol=n, dimnames=list(NULL, colnames(A))),
+             iter = re$maxiter)
     }
 
     # run nmf for given k and reps
@@ -65,9 +68,8 @@
 #' @param hpc_args   Optional arguments to pass to hpc module
 #' @return           A data frame with sample names, cluster membership, and score
 nmf = function(A, k, max_iter=1000, seed=1234, tolerance=1e-4, rep=10, hpc_args=NULL) {
-    # construct df here, call above func
-    result = .df$call(data.frame(k = k),
-                      .nmf,
-                      A=A, max_iter=max_iter, seed=seed, tolerance=tolerance,
-                      hpc_args=hpc_args)
+    .df$call(data.frame(k = k),
+             .nmf,
+             A=A, max_iter=max_iter, seed=seed, tolerance=tolerance,
+             hpc_args=hpc_args)
 }
