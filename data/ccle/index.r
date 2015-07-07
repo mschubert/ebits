cbdb29c3be140b17432f82238cf817970a0cfcf6
mschubert <mschu.dev@gmail.com>
2015-07-07 13:49:56 +0100

    working basic ccle module

--- data/ccle/index.r
+++ data/ccle/index.r
@@ -1,4 +1,5 @@
 .p = import('../path')
+.b = import('../../base')
 .io = import('../../io')
 .ar = import('../../array')
 .df = import('../../data_frame')
@@ -22,9 +23,22 @@ basal_expression = function(index_type="COSMIC") {
             ma$annotate("hgnc_symbol")
         save(expr, file=.p$file("ccle", fname, ext=".RData"))
     } else
-        expr = .p$load("ccle", fname, ext=".RData")
+        expr = .p$load("ccle", paste0(fname, ".RData"))
 
-#TODO: map identifiers here and return object
+    expr = Biobase::exprs(expr)
+    cn = .b$match(sub(".CEL$", "", colnames(expr)),
+                  from = index$Expression.arrays,
+                  to = index[[index_type]])
+    
+    colnames(expr) = unname(cn)
+    nas = is.na(cn)
+    if (any(nas)) {
+        warning("dropping ", sum(nas), " identifiers for ", index_type, " mapping")
+        expr = expr[,!nas]
+        cn = cn[!nas]
+    }
+
+    expr
 }
 
 #' Returns a drug response matrix (drugs x COSMIC IDs)
@@ -34,11 +48,19 @@ basal_expression = function(index_type="COSMIC") {
 #'
 #' @param index_type  The column of `index` to be used for column names
 #' @return            The drug response matrix
-drug_response = function(index_type="COSMIC") {
+drug_response = function(index_type="COSMIC", map_drugs=TRUE) {
     idx = select_(index, "CCLE.name", index=index_type)
-    .p$read("ccle", "CCLE_NP24.2009_Drug_data_2015.02.24.csv", header=TRUE) %>%
+    re = .p$read("ccle", "CCLE_NP24.2009_Drug_data_2015.02.24.csv", header=TRUE) %>%
         select(CCLE.name=CCLE.Cell.Line.Name, Compound, IC50=IC50..uM.) %>%
         .df$update(idx, match_cols="CCLE.name") %>%
         na.omit() %>%
         .ar$construct(IC50 ~ index + Compound, data=.)
+
+    if (map_drugs) {
+        fname = .io$file_path(module_file(), "drug_mapping.txt")
+        lookup = .io$read_table(fname, header=TRUE)
+        colnames(re) = lookup$GDSC_NAME[match(colnames(re),
+                lookup$CCLE_NAME)] %or% colnames(re)
+    }
+    re
 }
