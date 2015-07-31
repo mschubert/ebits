cee6d22e29aea061b01ab974646f158398b592b0
mschubert <mschu.dev@gmail.com>
2015-07-31 15:19:53 +0100

    add shuffle option

--- stats/export_indexed/ml.r
+++ stats/export_indexed/ml.r
@@ -7,16 +7,19 @@
 #' @param aggr        Return aggregate statistics instead of fit statistics
 #' @param xval        Number of cross-validation steps to perform
 #' @param min_pts     Minimum number of data points to calculate model from
+#' @param shuffle_labels  Whether to shuffle the response variable - for null models
 #' @param subsets     Vector whose unique set is used to split the data along rows
 #' @return            A data.frame containing the trained models
 ml = function(formula, train_args, data=environment(formula),
-              models=FALSE, aggr=FALSE, xval=5, min_pts=10) {
+              models=FALSE, aggr=FALSE, xval=5, min_pts=10, shuffle_labels=FALSE) {
     library(mlr) # needed for options
 
     # set all formula vars to atomic
     indep_vars = all.vars(formula[[3]])
     dep_vars = all.vars(formula[[2]])
-    stopifnot(length(dep_vars) == 1) # only allow one readout variable
+    stopifnot(length(dep_vars) == 1 && length(dim(dep_vars)) < 2) # only vector response
+    if (identical(shuffle_labels, TRUE))
+        data[[dep_vars]] = sample(data[[dep_vars]])
 
     # bind data and response to data.frame, use var name if one column
     for (i in seq_along(data))
