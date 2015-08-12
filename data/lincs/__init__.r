43f350f822be2622bd981ef948cc09f42432786b
mschubert <mschu.dev@gmail.com>
2015-08-12 13:10:40 +0100

    use biomart for lincs annotations

--- data/lincs/__init__.r
+++ data/lincs/__init__.r
@@ -1,6 +1,7 @@
 .b = import('../../base') # %or%
 .io = import('../../io')
 .p = import('../path')
+.ar = import('../../array')
 
 #' Landmark probes
 landmarks = as.character(read.table(file.path(module_file(),'rid_landmarks.txt'))$V1)
@@ -38,16 +39,18 @@ parse_gctx = function(fname, rid=NULL, cid=NULL) {
 #'
 #' @param cid        Vector of experiment IDs to subset
 #' @param rid        Vector of probe IDs to subset
-#' @param map.genes  BioMart identifier (eg. 'hgnc_symbol') of IDs to map to, or FALSE
+#' @param map.genes  BioMart identifier of IDs to map to, or FALSE. Supported
+#'                   identifiers are: hgnc_symbol, hgnc_id, entrezgene,
+#'                   ensembl_gene_id, ensembl_transcript_id
 get_z = function(cid, rid=landmarks, map.genes=FALSE) {
     #TODO: handle transpose better?
     fname = .p$file("lincs", "zspc_n1328098x22268.gctx")
     re = parse_gctx(fname=fname, cid=cid, rid=rid)
 
-    if (is.character(map.genes)) {
-        ann = import('../../process/microarray/annotate')
-        ann$annotate(re, annotation="hthgu133a.db", summarize=map.genes)
-    } else
+    if (is.character(map.genes))
+        .ar$summarize(re, along=1, from="affy_hg_u133_plus_2", to=map.genes,
+                      data=.probe_annotations(), FUN=function(x) mean(x, na.rm=TRUE))
+    else
         re
 }
 
@@ -57,4 +60,22 @@ get_z = function(cid, rid=landmarks, map.genes=FALSE) {
 get_index = function() {
     fname = .p$file("lincs", "inst.info")
     .io$read_table(fname, quote="", header=TRUE, sep="\t")
+}
+
+#' Returns a data.frame of probe annotations
+#'
+#' @return  A data.frame with different annotation columns
+.probe_annotations = function() {
+    fname = .p$file('lincs', 'annot.RData')
+    if (!file.exists(fname)) {
+        mart = biomaRt::useMart(biomart="ensembl", dataset="hsapiens_gene_ensembl")
+        annot = biomaRt::getBM(attributes = c("hgnc_symbol", "hgnc_id", "entrezgene",
+            "ensembl_gene_id", "ensembl_transcript_id", "affy_hg_u133_plus_2"),
+            filter = "affy_hg_u133_plus_2", values = projected, mart=mart)
+        annot[annot==''] = NA
+
+        save(annot, file=fname)
+        annot
+    } else
+        .io$load(fname)
 }
