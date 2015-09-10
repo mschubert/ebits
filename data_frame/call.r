663b52fed42ec79ca47fd883a2ab5f61d5327cbc
mschubert <mschu.dev@gmail.com>
2015-09-10 12:05:06 +0100

    always tidy if not only result returned

--- data_frame/call.r
+++ data_frame/call.r
@@ -4,11 +4,10 @@
 #' @param fun          The function to call
 #' @param ...          Further arguments to pass to `fun`
 #' @param result_only  Return only the result column instead of the data.frame
-#' @param tidy         Try to convert the result into a data.frame
 #' @param rep          How many times to repeat the calls (useful for sampling funcs)
 #' @param hpc_args     If not NULL, arguments to be passed to `hpc$Q`
 #' @return             A data frame with the function call results
-call = function(df, fun, ..., result_only=FALSE, tidy=TRUE, rep=FALSE, hpc_args=NULL) {
+call = function(df, fun, ..., result_only=FALSE, rep=FALSE, hpc_args=NULL) {
     if (!inherits(df, "IndexedCall"))
         stop("df needs to be created with df$create_[formula_]index")
 
@@ -38,20 +37,19 @@ call = function(df, fun, ..., result_only=FALSE, tidy=TRUE, rep=FALSE, hpc_args=
         result = do.call(import('../hpc')$Q, c(list(fun=fun, ...), index,
             hpc_args, const=list(df$args)))
 
-    names(result) = 1:length(result)
-    index$rep = add_rep
-
     if (!result_only) {
+        names(result) = 1:length(result)
+        index$rep = add_rep
+
         rownames(index) = as.character(1:nrow(index))
         result = lapply(names(result), function(i) {
-            if (is.null(names(result[[1]])))
+            if (is.null(names(result[[i]])))
                 c(as.list(index[i,,drop=FALSE]), result=as.list(result[[i]]))
             else
                 c(as.list(index[i,,drop=FALSE]), as.list(result[[i]]))
         })
+        result = dplyr::bind_rows(lapply(result, as.data.frame))
     }
-    if (tidy)
-        result = dplyr::rbind_all(lapply(result, as.data.frame))
 
     result
 }
