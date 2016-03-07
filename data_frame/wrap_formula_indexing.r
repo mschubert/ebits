mschubert/ebits

commit 00e22184a3000963dd7d9fca2e3b656b90b14b2f

use funcs from #84

data_frame/wrap_formula_indexing.r
@@ -29,11 +29,10 @@ wrap_formula_indexing = function(FUN) {
         func = import_('../base/functional')
         idx = import_('../base/indexing')
         df = import_('../data_frame')
-        call_args = as.list(func$match_call_defaults())[-1]
-        for (i in seq_along(call_args)) #TODO: add this in match_call_defaults()?
-            if (class(call_args[[i]]) %in% c("name", "call"))
-                call_args[[i]] = eval(call_args[[i]], envir=parent.frame())
+
+        call_args = as.list(func$eval_call(func$match_call_defaults()))[-1]
         call_args = call_args[!names(call_args) %in% c("rep","hpc_args")]
+
         idf = do.call(df$create_formula_index, call_args)
         df$call(idf, one_item, rep=rep, result_only=result_only, hpc_args=hpc_args)
     }

commit 0e107f46f9dba7019644e402262e9e80d1c5d42c

revert #84 to address #87, add test case
bug introduced in 00e22184a3000963dd7d9fca2e3b656b90b14b2f

data_frame/wrap_formula_indexing.r
@@ -30,7 +30,13 @@ wrap_formula_indexing = function(FUN) {
         idx = import_('../base/indexing')
         df = import_('../data_frame')
 
-        call_args = as.list(func$eval_call(func$match_call_defaults()))[-1]
+#FIXME: commented line below does not work
+        call_args = as.list(func$match_call_defaults())[-1]
+        for (i in seq_along(call_args))
+            if (class(call_args[[i]]) %in% c("name", "call"))
+                call_args[[i]] = eval(call_args[[i]], envir=parent.frame())
+
+#        call_args = as.list(func$eval_call())[-1]
         call_args = call_args[!names(call_args) %in% c("rep","hpc_args")]
 
         idf = do.call(df$create_formula_index, call_args)
@@ -66,4 +72,14 @@ if (is.null(module_name())) {
     expect_equal(nrow(re3), 4)
     expect_equal(setdiff(colnames(width), re3$width), character(0))
     expect_equal(setdiff(colnames(length), re3$length), character(0))
+
+    # integrative test using linear model
+    lm_fun = function(x, index) {
+        st = import('stats')
+        mod = st$lm(x ~ 0 + Species, data=index)
+    }
+    re = lm_fun(x=iris[1:3], index=iris['Species'])
+    expect_equal(unique(re$term), paste0("Species", unique(iris$Species)))
+    expect_equal(unique(re$x), colnames(iris)[1:3])
+    expect_equal(unique(re$size), 50)
 }