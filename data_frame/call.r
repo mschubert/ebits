a8c0d7e9a136e8c2c79d85068ba1b3f831edd4ea
mschubert <mschu.dev@gmail.com>
2016-09-14 11:23:17 +0100

    whitespace

--- data_frame/call.r
+++ data_frame/call.r
@@ -70,21 +70,21 @@ call = function(df, fun, ..., result_only=FALSE, rep=FALSE, hpc_args=NULL) {
 
 if (is.null(module_name())) {
     library(testthat)
-	b = import('../base')
+    b = import('../base')
 
-	x = 1:2
-	y = 3:4
-	z = 10
+    x = 1:2
+    y = 3:4
+    z = 10
 
-	f1 = function(x,y) x+y
-	r1 = b$expand_grid(x=x,y=y) %>% call(f1)
+    f1 = function(x,y) x+y
+    r1 = b$expand_grid(x=x,y=y) %>% call(f1)
 
-	f2 = function(x,y,z) x+y+z
-	r2 = b$expand_grid(x=x,y=y) %>% call(f2, z=z)
+    f2 = function(x,y,z) x+y+z
+    r2 = b$expand_grid(x=x,y=y) %>% call(f2, z=z)
 
-	expect_equal(r1[c('x','y')], r2[c('x','y')])
-	expect_equal(r1$result, rowSums(expand.grid(x,y)))
-	expect_equal(r2$result, z + rowSums(expand.grid(x,y)))
+    expect_equal(r1[c('x','y')], r2[c('x','y')])
+    expect_equal(r1$result, rowSums(expand.grid(x,y)))
+    expect_equal(r2$result, z + rowSums(expand.grid(x,y)))
 
     # test correct ordering of empty results
     ferr = function(x) if (x %% 2 == 0) x
