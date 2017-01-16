ad6dbc92c7b21857be85f6e6e5cf93561e0f97a7
mschubert <mschu.dev@gmail.com>
2017-01-16 21:18:37 +0000

    look for object in right frame

--- io/binary.r
+++ io/binary.r
@@ -42,7 +42,7 @@ save = function(..., file) {
         else
             .l1k$write.gct(obj, file)
     } else
-        base::save(..., file=file, envir=parent.frame(2L))
+        base::save(..., file=file, envir=parent.frame())
 }
 
 #' Function to load all files that match a regular expression
