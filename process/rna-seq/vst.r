905819ab473df2c851a23eea8bb8ed2bd5c90a26
mschubert <mschu.dev@gmail.com>
2017-01-17 15:54:16 +0000

    fix vst

--- process/rna-seq/vst.r
+++ process/rna-seq/vst.r
@@ -2,18 +2,24 @@
 #'
 #' @param mat  The untransformed matrix [genes x samples]
 #' @return     A matrix of gene expression values [genes x samples]
-vst = function(mat, conditions=NULL) UseMethod("vst")
-
-#' @rdname vst
-vst.default = function(mat, conditions) {
-	cds = DEseq2::newCountDataSet(countData=dat, conditions=factor(conditions))
-	cds = DEseq2::estimateSizeFactors(cds)
-	cds = DEseq2::estimateDispersions(cds, sharingMode="gene-est-only",
-		method = "pooled", fitType = "local")
-	vst = DEseq2::getVarianceStabilizedData(cds)
+vst = function(mat, colData=data.frame(id=colnames(mat)), design=~1) {
+    UseMethod("vst")
 }
 
 #' @rdname vst
-vst.list = function(mlist) {
-	lapply(mlist, vst)
+vst.matrix = function(mat, colData=data.frame(id=colnames(mat)), design=~1) {
+    cds = DESeq2::DESeqDataSetFromMatrix(mat, colData, design)
+    vst = vst(cds)
+}
+
+#' @rdname vst
+vst.DESeqDataSet = function(cds) {
+    cds = DESeq2::estimateSizeFactors(cds)
+    cds = DESeq2::estimateDispersions(cds)
+    vst = DESeq2::getVarianceStabilizedData(cds)
+}
+
+#' @rdname vst
+vst.list = function(mlist, ...) {
+	lapply(mlist, vst, ...)
 }
