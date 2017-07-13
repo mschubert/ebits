1d84e7a9322ce158043e3ae23216d5d81fc3f9f9
mschubert <mschu.dev@gmail.com>
2017-07-13 12:16:48 +0100

    respect text size for axis labels

--- plot/matrix.r
+++ plot/matrix.r
@@ -59,7 +59,8 @@ matrix = function(df, formula, color="color", label=NULL, palette="RdYlGn", text
         panel.background = element_blank(),
         panel.grid.minor = element_blank(),
         panel.grid.major = element_blank(),
-        axis.text.x = element_text(angle=45, hjust=1))
+        axis.text.x = element_text(angle=45, hjust=1, size=text_size),
+        axis.text.y = element_text(size=text_size))
     )
 
     p = ggplot(df, aes_string(x=cols, y=rows, fill=value))
