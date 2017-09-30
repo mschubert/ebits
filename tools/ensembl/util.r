3e8335194a09c4732b35b0b44c910c91c4b4e16f
mschubert <mschu.dev@gmail.com>
2017-09-30 15:46:39 +0200

    add GET, POST util

---
+++ tools/ensembl/util.r
@@ -0,0 +1,38 @@
+#' Send HTTP GET request
+#'
+#' @param uri  URI of the endpoint, or parts thereof as character vector
+#' @param ...  Key-value pairs that will be encoded appropriately
+#' @return     Named list of values
+get = function(uri=c(), ...) {
+    server = "http://rest.ensembl.org"
+    ext = paste(uri, collapse="/")
+    url = paste(server, ext, sep="/")
+
+    req = httr::GET(url, query=list(...),
+                    httr::content_type("application/json"))
+
+    if (!req$status == 200)
+        stop("HTTP query error: expected 200 OK, got ", req$status)
+
+    rjson::fromJSON(intToUtf8(req$content))
+}
+
+#' Send HTTP POST request
+#'
+#' @param uri  URI of the endpoint, or parts thereof as character vector
+#' @param ...  Key-value pairs that will be encoded appropriately
+#' @return     Named list of values
+post = function(uri=c(), ...) {
+    server = "http://rest.ensembl.org"
+    ext = paste(uri, collapse="/")
+    url = paste(server, ext, sep="/")
+
+    req = httr::POST(url, body=rjson::toJSON(list(...)),
+                     httr::content_type("application/json"),
+                     httr::accept("application/json"))
+
+    if (!req$status == 200)
+        stop("HTTP query error: expected 200 OK, got ", req$status)
+
+    rjson::fromJSON(intToUtf8(req$content))
+}
