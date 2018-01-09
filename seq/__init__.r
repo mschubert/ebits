0e6808c0bb0f87536cc9880e8f7ea91d73d80c9a
mschubert <mschu.dev@gmail.com>
2018-01-09 14:03:41 +0000

    keep server side

--- seq/__init__.r
+++ seq/__init__.r
@@ -11,5 +11,29 @@ probeset_table = import('./probeset_table')$probeset_table
 gene_table = import('./gene_table')$gene_table
 coords = import('./coords')
 
-info = GenomicRanges::seqinfo
+names = GenomeInfoDb::seqnames
 lengths = GenomeInfoDb::seqlengths
+levels = GenomeInfoDb::seqlevels
+info = GenomeInfoDb::seqinfo
+`info<-` = function(x, value) {
+    idx = unique(as.character(GenomeInfoDb::seqnames(x)))
+    x@seqinfo = value[idx]
+    if (inherits(x, "GRanges"))
+        x@seqnames = droplevels(x@seqnames)
+    x
+}
+
+set_info = function(x, name, info, drop=FALSE, envir=parent.frame()) {
+    # check that all levels in x are present in info, error otherwise
+
+    # assign info and seqlevels in x
+    idx = unique(as.character(GenomeInfoDb::seqnames(x)))
+    x@seqinfo = info(x)[idx]
+
+    # GenomeInfoDb has character vector for seqnames, GRanges has Factor-Rle
+    if (inherits(x, "GRanges"))
+        x@seqnames = droplevels(x@seqnames)
+
+    # return new seqinfo obj
+    assign(name, x, envir=envir)
+}
