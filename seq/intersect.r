1a76bedd56b52ba6fff280aef18900ca14b5ad34
mschubert <mschu.dev@gmail.com>
2018-01-29 09:38:55 +0000

    add more findOverlaps args

--- seq/intersect.r
+++ seq/intersect.r
@@ -2,11 +2,19 @@
 
 #' Intersect two GRanges objects
 #'
+#' See GenomicRanges/IRanges::findOverlaps for detailed arg docs
+#'
 #' @param query       GRanges object used as query
 #' @param subject     GRanges object used as subject
+#' @param type        Type of the overlap, eg. 'any', 'start', 'end', 'within'
+#' @param minoverlap  Minimum number of positions that need to overlap to count
+#' @param select      Which match to return (eg. 'first', 'last'; default: 'all')
 #' @param keep_index  Return indices in query and subject
+#' @param ...         Further arguments passed to findOverlaps
 #' @return            Merged data.frame based on overlaps
-intersect = function(query, subject, keep_index=FALSE) {
+intersect = function(query, subject, type="any", minoverlap=0L,
+                     select="all", keep_index=FALSE, ...) {
+
     cols_query = GenomicRanges::mcols(query) %>%
         as.data.frame() %>%
         dplyr::mutate(queryHits = seq_len(nrow(.)))
@@ -15,7 +23,8 @@ intersect = function(query, subject, keep_index=FALSE) {
         as.data.frame() %>%
         dplyr::mutate(subjectHits = seq_len(nrow(.)))
 
-    re = IRanges::findOverlaps(query, subject)
+    re = GenomicRanges::findOverlaps(
+        query, subject, type=type, minoverlap=minoverlap, select=select, ...)
 #    re$width = IRanges::ranges(re, query, subject)$width
     re = re %>%
         as.data.frame() %>%
