881a2b47f908130c0f21f700f8932193dbd0b5c0
mschubert <mschu.dev@gmail.com>
2018-01-29 09:30:20 +0000

    add option to keep index

--- seq/intersect.r
+++ seq/intersect.r
@@ -1,22 +1,29 @@
-library(dplyr)
+.ops = import('../base/operators')
 
 #' Intersect two GRanges objects
 #'
-#' @param query    GRanges object used as query
-#' @param subject  GRanges object used as subject
-#' @return         Merged data.frame based on overlaps
-intersect = function(query, subject) {
+#' @param query       GRanges object used as query
+#' @param subject     GRanges object used as subject
+#' @param keep_index  Return indices in query and subject
+#' @return            Merged data.frame based on overlaps
+intersect = function(query, subject, keep_index=FALSE) {
     cols_query = GenomicRanges::mcols(query) %>%
         as.data.frame() %>%
-        mutate(queryHits = seq_len(nrow(.)))
+        dplyr::mutate(queryHits = seq_len(nrow(.)))
 
     cols_subject = GenomicRanges::mcols(subject) %>%
         as.data.frame() %>%
-        mutate(subjectHits = seq_len(nrow(.)))
+        dplyr::mutate(subjectHits = seq_len(nrow(.)))
 
-    IRanges::findOverlaps(query, subject) %>%
+    re = IRanges::findOverlaps(query, subject)
+#    re$width = IRanges::ranges(re, query, subject)$width
+    re = re %>%
         as.data.frame() %>%
-        left_join(cols_query, by="queryHits") %>%
-        left_join(cols_subject, by="subjectHits") %>%
-        select(-queryHits, -subjectHits)
+        dplyr::left_join(cols_query, by="queryHits") %>%
+        dplyr::left_join(cols_subject, by="subjectHits")
+
+    if (keep_index)
+        re
+    else
+        dplyr::select(re, -queryHits, -subjectHits)
 }
