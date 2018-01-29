7fd0eaecd68276f679b0ac7b9fe04d252301d5e1
mschubert <mschu.dev@gmail.com>
2018-01-29 10:57:02 +0000

    fix overlap iwdth

--- seq/intersect.r
+++ seq/intersect.r
@@ -9,11 +9,12 @@
 #' @param type        Type of the overlap, eg. 'any', 'start', 'end', 'within'
 #' @param minoverlap  Minimum number of positions that need to overlap to count
 #' @param select      Which match to return (eg. 'first', 'last'; default: 'all')
+#' @param return_widths  Return width of match and fraction of query covered
 #' @param keep_index  Return indices in query and subject
 #' @param ...         Further arguments passed to findOverlaps
 #' @return            Merged data.frame based on overlaps
 intersect = function(query, subject, type="any", minoverlap=0L,
-                     select="all", keep_index=FALSE, ...) {
+                     select="all", return_widths=FALSE, keep_index=FALSE, ...) {
 
     cols_query = GenomicRanges::mcols(query) %>%
         as.data.frame() %>%
@@ -25,13 +26,20 @@ intersect = function(query, subject, type="any", minoverlap=0L,
 
     re = GenomicRanges::findOverlaps(
         query, subject, type=type, minoverlap=minoverlap, select=select, ...)
-    # ranges() is deprecated in IRanges, should replace when GRanges supports
-    #FIXME: this does not separate by seqnames, hence is wrong and disabled
-    #re$width = IRanges::overlapsRanges(query@ranges, subject@ranges)@width
+
     re = re %>%
         as.data.frame() %>%
         dplyr::left_join(cols_query, by="queryHits") %>%
         dplyr::left_join(cols_subject, by="subjectHits")
+
+    if (return_widths) {
+        overlaps = GenomicRanges::pintersect(
+            query[re$queryHits], subject[re$subjectHits])
+
+        re = re %>%
+            dplyr::mutate(query_width = GenomicRanges::width(query[re$queryHits]),
+                ov_frac = GenomicRanges::width(overlaps) / query_width)
+    }
 
     if (keep_index)
         re
