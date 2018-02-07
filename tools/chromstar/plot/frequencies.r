ff326750006dd07e1a1dd565ee9b3c223f7d6cdf
mschubert <mschu.dev@gmail.com>
2018-02-07 10:05:51 +0000

    plot freqs, not domains

--- tools/chromstar/plot/frequencies.r
+++ tools/chromstar/plot/frequencies.r
@@ -2,10 +2,15 @@ import_package('ggplot2', attach=TRUE)
 
 #' Plot genomic frequencies of states
 #'
+#' We extract from the model because chromstaR::genomicFrequencies is only
+#' showing the number of bins, not the fraction of the genome covered (if
+#' per.mark=FALSE; otherwise, it includes the width - don't know why)
+#'
 #' @param model  Chromstar model object
+#' @return       ggplot2 object (bar graph)
 frequencies = function(model) {
     freqs = chromstaR::genomicFrequencies(model)
-    fdf = reshape2::melt(as.array(freqs$domains))
+    fdf = reshape2::melt(as.array(freqs$frequency))
     p = ggplot(fdf, aes(x=Var1, y=value)) +
         scale_y_log10() +
         labs(x="modification", y="number") +
