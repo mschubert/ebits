4686092c890c6042f455ec26757806e28e187a70
mschubert <mschu.dev@gmail.com>
2018-02-19 10:38:26 +0000

    supply chrs or exclude

--- seq/aneuploidy.r
+++ seq/aneuploidy.r
@@ -6,27 +6,33 @@ import_package('dplyr', attach=TRUE)
 #' @param ranges  GRanges object or data.frame with copy numbers
 #' @param per_chromosome  Calculate aneuploidy score per chromosome or genome
 #' @param assembly  Character string of ID or GenomeInfoDb assembly object
+#' @param chromosomes  Character vector of chromosomes to use
+#' @param chr_exclude  Character vector of chromosomes to exclude (only used
+#'      if chromosomes=NULL)
 #' @return        Aneuploidy scores for each sample in `ranges`
 aneuploidy = function(ranges, assembly, per_chromosome=FALSE,
-                      chr_exclude = c("X", "Y", "MT"), ...) {
+            chromosomes=NULL, chr_exclude = c("X", "Y", "MT"), ...) {
     UseMethod("aneuploidy")
 }
 
 #' @rdname aneuploidy
 aneuploidy.GRanges = function(ranges, per_chromosome=FALSE, assembly="GRCh38",
-                              chr_exclude = c("X", "Y", "MT")) {
+            chromosomes=NULL, chr_exclude = c("X", "Y", "MT")) {
     aneuploidy(as.data.frame(ranges))
 }
 
 #' @rdname aneuploidy
 aneuploidy.data.frame = function(ranges, per_chromosome=FALSE, assembly="GRCh38",
-         chr_exclude = c("X", "Y", "MT"), width="width",
+         chromosomes=NULL, chr_exclude = c("X", "Y", "MT"), width="width",
          chromosome="seqnames", copies="ploidy", sample="Sample") {
     chrs = .lengths$chr_lengths(assembly)
     chrs = data.frame(seqnames=names(chrs), size=unname(as.numeric(chrs)))
 
+    if (is.null(chromosomes))
+        chromosomes = setdiff(ranges$seqnames, chr_exclude)
+
     cna = ranges %>%
-        filter(! seqnames %in% chr_exclude) %>%
+        filter(seqnames %in% chromosomes) %>%
         mutate(width = as.numeric(width),
                euploid_dev = abs(2 - ploidy)) %>%
         group_by(Sample, seqnames) %>%
