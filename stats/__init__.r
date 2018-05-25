addd9f3d15fa4a00fb23bed888f03d7ba9f0afbb
mschubert <mschu.dev@gmail.com>
2018-05-25 08:49:42 +0000

    add signed ks

--- stats/__init__.r
+++ stats/__init__.r
@@ -6,6 +6,7 @@ cor = import('./correlation')
 discretize = import('./discretize')
 fishers_exact_test = import('./fishers_exact_test')$fishers_exact_test
 hypergeometric_test = import('./hypergeometric_test')$hypergeometric_test
+signed_ks_test = import('./signed-ks-test')$ks.test.2
 
 .wrap = import('../data_frame/wrap_formula_indexing')
 for (fname in list.files(module_file('export_indexed'), "\\.r$", recursive=TRUE)) {
