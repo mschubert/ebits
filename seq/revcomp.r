8acfaddf9106acbbfb32247c5a9cae6364f99cfb
mschubert <mschu.dev@gmail.com>
2018-07-30 11:20:49 +0000

    import function for partial

--- seq/revcomp.r
+++ seq/revcomp.r
@@ -1,10 +1,11 @@
 # Sequence analysis basics
+.f = import('../base/functional')
 
 #' Reverse complement sequences
 revcomp = function (strs) {
     rc = function (str) {
         bases = strsplit(str, '')[[1]]
-        compl = vapply(bases, partial(switch, A='T', C='G', G='C', T='A'), '')
+        compl = vapply(bases, .f$partial(switch, A='T', C='G', G='C', T='A'), '')
         paste(rev(compl), collapse = '')
     }
     vapply(strs, rc, '')
