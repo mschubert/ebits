6c4ca99e108f705fedb2411bd46cf635203c5bd8
mschubert <mschu.dev@gmail.com>
2018-12-04 12:36:51 +0000

    add test for covar, grouping

--- stats/export_indexed.r
+++ stats/export_indexed.r
@@ -2,6 +2,7 @@ import(".", attach=TRUE)
 
 if (is.null(module_name())) {
     library(testthat)
+    library(dplyr)
 
     a = matrix(c(2,0,1,0,1,0), ncol=2, dimnames=list(c('x','y','z'), c('a','b')))
     b = setNames(c(1,2,0), c('x','y','z'))
@@ -14,4 +15,13 @@ if (is.null(module_name())) {
     fit2 = lm(b ~ a, atomic="a")
     expect_equal(nrow(fit2), 2)
     expect_equal(fit2$term, c('aa','ab'))
+
+    c = matrix(c(1,1,2,2,1,1), ncol=2, dimnames=dimnames(a))
+    fit3 = lm(a ~ b + c)
+    idx = tidyr::crossing(c=colnames(c), a=colnames(a), term=c('b','c'))[c('a','c','term')]
+    expect_equal(tbl_df(fit3[c('a','c','term')]), idx)
+
+    fit4 = lm(a ~ c + b, group=c('a', 'c'))
+    expect_equal(fit4 %>% arrange(a,c,term),
+                 dplyr::filter(fit3, a==c) %>% arrange(a,c,term))
 }
