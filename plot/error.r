57701a96c4524edc56f37b658e30773bb6a974c0
mschubert <mschu.dev@gmail.com>
2019-04-25 09:50:43 +0200

    fix error plotting

--- plot/error.r
+++ plot/error.r
@@ -3,7 +3,7 @@
 #' @param label  Character vector of the message
 #' @return  empty ggplot2 object with only the label
 error = function(label) {
-    ggplot(data.frame()) +
-        geom_text(x=1, y=1, label=label) +
+    ggplot(data.frame(x=1, y=1)) +
+        geom_text(aes(x=x, y=y), label=label) +
         theme_void()
 }
