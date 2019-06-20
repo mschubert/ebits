5e4a354f365db0f430ba43905a7e3afacf159c78
mschubert <mschu.dev@gmail.com>
2019-06-20 10:53:05 +0200

    also add pos/neg label bias

--- plot/volcano.r
+++ plot/volcano.r
@@ -20,11 +20,12 @@ color = import('./color')
 #' @param ylim       Limits along the vertical axis; default: fit data
 #' @param simplify   Drop some insignificant points and labels to reduce file size
 #' @param repel      Whether to use repel package for labels
-#' @param x_label_bias  Multiplier to focus more on effect size than significance
+#' @param x_label_bias   Multiplier to focus more on effect size than significance
+#' @param pos_label_bias Multiplier to focus more on positive than negative labels
 #' @return           A ggplot2 object of the volcano plot
 volcano = function(df, base.size=1, p=0.05, label_top=20, ceil=0, check_overlap=FALSE,
         text.size=3.5, xlim=c(NA,NA), ylim=c(NA,NA), simplify=TRUE, repel=FALSE,
-        x_label_bias=1) {
+        x_label_bias=1, pos_label_bias=1) {
     if (!'label' %in% colnames(df))
         stop("Column 'label' not found. You need to specify a label for your points")
     if (!'color' %in% colnames(df))
@@ -54,6 +55,7 @@ volcano = function(df, base.size=1, p=0.05, label_top=20, ceil=0, check_overlap=
     rel_effect = df$.x/max(abs(df$.x), na.rm=TRUE)
     rel_pval = 2 * log10(df$.y) / min(log10(df$.y), na.rm=TRUE)
     point_dist = rel_effect^2 * x_label_bias + rel_pval^2
+    point_dist[rel_effect > 0] = point_dist[rel_effect > 0] * pos_label_bias
     point_dist[is.na(df$label)] = NA # only keep points where we have labels
     df$label[rank(-point_dist) > label_top & !df$circle] = NA
 
