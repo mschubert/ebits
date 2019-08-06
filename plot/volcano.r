ef96d4e1617ec0f70d6275602806f17615e14a77
mschubert <mschu.dev@gmail.com>
2019-08-06 21:24:02 +0200

    more repel iterations

--- plot/volcano.r
+++ plot/volcano.r
@@ -91,7 +91,7 @@ volcano = function(df, base.size=1, p=0.05, label_top=20, ceil=0, check_overlap=
 
     if (repel)
         p + ggrepel::geom_text_repel(mapping = aes(x = .x, y = .y, label = label),
-                colour = "#353535", size = text.size, na.rm = TRUE, segment.alpha=0.5)
+                colour= "#353535", size = text.size, na.rm = TRUE, segment.alpha=0.5, max.iter=20000)
     else
         p + geom_text(mapping = aes(x = .x, y = .y, label = label),
                 colour = "#353535", size = text.size, vjust = -1, na.rm = TRUE, check_overlap=check_overlap)
