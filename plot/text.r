0ae0d0d2311c2014aec79d2871d97e1aa74af97b
mschubert <mschu.dev@gmail.com>
2019-08-09 15:23:23 +0200

    pass ... to geom_text

--- plot/text.r
+++ plot/text.r
@@ -1,9 +1,10 @@
 #' Plot a simple text message
 #'
 #' @param label  Character vector of the message
+#' @param ...    Paramters passed to geom_text
 #' @return  empty ggplot2 object with only the label
-text = function(label) {
+text = function(label, ...) {
     ggplot(data.frame(x=1, y=1)) +
-        geom_text(aes(x=x, y=y), label=label) +
+        geom_text(aes(x=x, y=y), label=label, ...) +
         theme_void()
 }
