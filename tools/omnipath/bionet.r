760883a2e0e58874cb2e50e4253710cd2d63d267
mschubertv <mschu.dev@gmail.com>
2023-09-07 08:55:16 +0000

    add automatic threshold

--- tools/omnipath/bionet.r
+++ tools/omnipath/bionet.r
@@ -1,24 +1,36 @@
 import_package("tidygraph", attach=TRUE)
 
+#' Automatically compute threshold based on variable and number of genes
+#'
+#' @inherit bionet
+autothresh = function(assocs, var, n_genes) {
+    sort(assocs[[var]])[n_genes]
+}
+
 #' Return BioNet Steiner Tree subnetwork
 #'
 #' @param g  tidygraph-compatible network
 #' @param assocs  data.frame with fields: n_smp, p.value, adj.p
 #' @param thresh  p-value/fdr cutoff
+#' @param n_genes  If non-null, thresh is calculated from how many genes to include
+#' @param var  Which column in 'assocs' to use for ranking
 #' @return  tidygraph object
-bionet = function(g, assocs, thresh=0.05, var=c("adj.p", "padj")) {
+bionet = function(g, assocs, thresh=0.05, n_genes=NULL, var=c("adj.p", "padj")) {
     if (is.null(assocs$name) || any(duplicated(assocs$name)))
         stop("[tools/omnipath]: bionet needs 'assocs' with unique 'name' field")
     if (length(var) > 1) {
         var = var[1]
         message("[tools/omnipath]: using ", sQuote(var), " for scoring")
     }
+    if (!is.null(n_genes))
+        tresh = autothresh(assocs, var, n_genes)
 
     g = g %>% activate(nodes) %>% left_join(assocs)
     scores = setNames(pull(g, !! rlang::sym(var)), pull(g, name))
     scores[is.na(scores)] = 1
     scores = pmax(-log10(scores) + log10(thresh), 0)
-    as_tbl_graph(BioNet::runFastHeinz(g, scores)) %>%
+    BioNet::runFastHeinz(g, scores) %>%
+        as_tbl_graph() %>%
         activate(edges) %>%
         filter(from != to)
 }
